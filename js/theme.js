1f236724a35f2560e5e5bb27cead61dc1369bdfb
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2024-05-01 16:58:01 +0100

    save theme setting to local storage

--- js/theme.js
+++ js/theme.js
@@ -2,18 +2,32 @@ const themeSwitch = document.querySelector('.theme-switch');
 const themeIcon = themeSwitch.querySelector('img');
 const body = document.querySelector('body');
 
+window.addEventListener('DOMContentLoaded', (e) => {
+  const lsTheme = localStorage.getItem('deividas.blog-theme');
+
+  if (lsTheme !== null) {
+    body.classList.add(lsTheme);
+  } else {
+    body.classList.add('light');
+  }
+});
+
 themeSwitch.addEventListener('click', (e) => {
+  // Switch to dark theme
   if (body.classList.contains('light')) {
     body.classList.remove('light');
     body.classList.add('dark');
     themeIcon.src = '/assets/icons/sun-icon.svg';
+    localStorage.setItem('deividas.blog-theme', 'dark');
     return;
   }
 
+  // Switch to light theme
   if (body.classList.contains('dark')) {
     body.classList.remove('dark');
     body.classList.add('light');
     themeIcon.src = '/assets/icons/moon-icon.svg';
+    localStorage.setItem('deividas.blog-theme', 'light');
     return;
   }
 });
